d39e150a83ccbfeaa9f46b7bcc4e2a2f50df4385
Nosu♡ <93537876+Nosu-u@users.noreply.github.com>
2022-09-05 02:52:53 +0200

    cool

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <Geode.hpp>
+
+USE_GEODE_NAMESPACE();
+
+CCLabelBMFont* g_atts;
+
+class $modify(PlayLayer) {
+	bool init(GJGameLevel* gj) {
+		g_atts = CCLabelBMFont::create("Atts: ??", "bigFont.fnt");
+
+		PlayLayer::init(gj);
+
+		auto corner = CCDirector::sharedDirector()->getScreenTop();
+		g_atts->setPosition(500 , corner - 35);
+		g_atts->setScale(0.5);
+		g_atts->setAnchorPoint({0, 0.5});
+		g_atts->setOpacity(100);
+		auto work = m_currentAttempt;
+		char str[64];
+		sprintf(str, "Atts: %0d", work);
+		g_atts->setString(str);
+
+		addChild(g_atts, 1000);
+
+		return true;
+	}
+
+	void delayedResetLevel() {
+		PlayLayer::delayedResetLevel();
+		auto work = m_currentAttempt;
+		char str[64];
+		sprintf(str, "Atts: %0d", work);
+		g_atts->setString(str);
+	}
+};
+
+
+
+
+
+
+
+
